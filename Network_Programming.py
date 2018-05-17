1a0beeaa67c8fa74adca271c16138e53e4547b47
omar <omaroobaniessa@gmail.com>
2018-05-17 11:29:17 +0100

    delted 1 file

--- Network_Programming.py
+++ Network_Programming.py
@@ -8,6 +8,37 @@ while True:
     data = mysock.recv(512)
     if (len(data) < 1):
         break
-    print(data.decode(),end='')
+    print(data.decode())
 mysock.close()
 #"""""""""""""""""""""""""
+import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen('https://www.facebook.com/dyi/?x=AdkZoO2GQNigIwe-&tab=all_archives&referrer=notif&notif_id=1526488266354079&notif_t=dyi')
+for line in fhand:
+    print(line.decode().strip())
+###############
+from bs4 import BeautifulSoup
+import ssl
+
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+
+# html.parser is the HTML parser included in the standard Python 3 library.
+# information on other HTML parsers is here:
+# http://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-a-parser
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    # Look at the parts of a tag
+    print('TAG:', tag)
+    print('URL:', tag.get('href', None))
+    print('Contents:', tag.contents[0])
+    print('Attrs:', tag.attrs)
